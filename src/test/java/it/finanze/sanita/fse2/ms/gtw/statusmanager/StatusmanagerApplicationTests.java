5db94ad1d88f1fcc818cb80850a45e1d93040a10
Simone <39314951+simone-lungarella@users.noreply.github.com>
2022-05-26 18:12:52 +0200

    fix(test): disabled useless test

--- src/test/java/it/finanze/sanita/fse2/ms/gtw/statusmanager/StatusmanagerApplicationTests.java
+++ src/test/java/it/finanze/sanita/fse2/ms/gtw/statusmanager/StatusmanagerApplicationTests.java
@@ -1,9 +1,11 @@
 package it.finanze.sanita.fse2.ms.gtw.statusmanager;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class StatusmanagerApplicationTests {
 
 	@Test
