9ef41ae1d86b7168c76315fd9de7c08be922c414
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2022-09-29 10:41:16 +0200

    feat(Kafka) : Add kafka log

---
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/statusmanager/utility/StringUtility.java
@@ -0,0 +1,24 @@
+package it.finanze.sanita.fse2.ms.gtw.statusmanager.utility;
+
+import com.google.gson.Gson;
+
+public final class StringUtility {
+
+	/**
+	 * Private constructor to avoid instantiation.
+	 */
+	private StringUtility() {
+		// Constructor intentionally empty.
+	}
+
+	/**
+	 * Transformation from Object to Json.
+	 * 
+	 * @param obj	object to transform
+	 * @return		json
+	 */
+	public static String toJSON(final Object obj) {
+		return new Gson().toJson(obj);
+	}
+
+}
