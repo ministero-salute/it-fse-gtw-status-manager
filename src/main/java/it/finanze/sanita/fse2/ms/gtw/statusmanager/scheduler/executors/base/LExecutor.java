540e247fb5639ddeeb2c5d499585f0d1cf7af2e4
Simone Le Noci <simonelenocidrum@gmail.com>
2022-11-18 14:42:53 +0100

    feat : added header license

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/statusmanager/scheduler/executors/base/LExecutor.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/statusmanager/scheduler/executors/base/LExecutor.java
@@ -1,3 +1,6 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 package it.finanze.sanita.fse2.ms.gtw.statusmanager.scheduler.executors.base;
 
 import it.finanze.sanita.fse2.ms.gtw.statusmanager.enums.ActionRes;
