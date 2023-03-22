7da1ebf8b8a3e0cf99323b103f022206443fdf9d
Simone Le Noci <simonelenocidrum@gmail.com>
2023-03-22 17:27:59 +0100

    feat(transaction): added extra field

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/statusmanager/repository/entity/FhirEvent.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/statusmanager/repository/entity/FhirEvent.java
@@ -3,13 +3,14 @@
  */
 package it.finanze.sanita.fse2.ms.gtw.statusmanager.repository.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Date;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.util.Date;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Document(collection = "#{@transactionDataBean}")
 @Data
@@ -25,6 +26,8 @@ public class FhirEvent {
     public static final String FIELD_EVENT_TYPE = "eventType";
     public static final String FIELD_EVENT_STATUS = "eventStatus";
     public static final String FIELD_EXPIRING_DATE = "expiring_date";
+    public static final String FIELD_EXTRA = "extra";
+    
     @Id
     private String id;
     @Field(name = FIELD_WIF)
@@ -37,6 +40,8 @@ public class FhirEvent {
     private String status;
     @Field(name = FIELD_EXPIRING_DATE)
     private Date expiringDate;
+    @Field(name = FIELD_EXTRA)
+    private String extra;
     
 
     public static FhirEvent asSuccess(String wif, Date date, Date expiringDate) {
