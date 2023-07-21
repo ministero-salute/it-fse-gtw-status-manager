424aab533e9ce0f91a7638ba47416225f1de42f5
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2023-07-21 12:28:30 +0200

    fix(deps): Remove snakeyaml deps

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/statusmanager/client/impl/ProcessorClient.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/statusmanager/client/impl/ProcessorClient.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.yaml.snakeyaml.scanner.Constant;
 
 import java.time.OffsetDateTime;
 
